c50d6c3464f2f9d0dd90562066b91e18fee6c3f1
haider <haider_al@me.com>
2024-05-21 20:07:08 -0700

    Renamed class to Die for containing each individual die. Created a Dice class to hold batches of Die.

--- src/dice.py
+++ src/dice.py
@@ -1,10 +1,13 @@
 import random
+import operator
 
 
 class Die:
     def __init__(self, sides: int):
         self.current = None
+        self.modified_current = None
         self.rolled = []
+        self.modified_rolls = []
         self.sides = sides
 
     def roll(self):
@@ -16,3 +19,16 @@ class Die:
         if len(self.rolled) > 0:
             self.current = self.rolled[-1]
 
+    def modify_roll(self, operator, number):
+        result = operator(self.current, number)
+        self.modified_current = result
+        self.modified_rolls.append(result)
+
+
+class Dice:
+    def __init__(self):
+        pass
+    def roll(self, count, sides):
+        dice_list = [Die(sides) for _ in range (3)]
+
+        return dice_list
